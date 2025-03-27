f128ba9d4a1439f2cf1937d7b463a8a83ef3c42c
amirjavani <amirjavani@outlook.com>
2025-03-27 23:18:51 +0330

    starter

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {Helmet} from "react-helmet";
+
+function App() {
+
+  return (
+    <div className="text-center items-center text-6xl mt-60">
+      <Helmet>
+        <title>صفحه اصلی | فرانت کست</title>
+      </Helmet>
+      front cast
+    </div>
+  )
+}
+
+export default App
