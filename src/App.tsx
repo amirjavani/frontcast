d74e67be1d8a50da357a3355d106943ac74b5a74
amirjavani <amirjavani@outlook.com>
2025-03-29 16:48:47 +0330

    adding fonts and icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import {Helmet} from "react-helmet";
 
 function App() {
-
+  
   return (
-    <div className="text-center items-center text-6xl mt-60">
+    <div className="text-center items-center text-6xl mt-60 main-font"  >
       <Helmet>
         <title>صفحه اصلی | فرانت کست</title>
+
       </Helmet>
       front cast
+
     </div>
   )
 }
