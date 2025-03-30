42ba275e7794b531587896c5359f0433e21b9ada
amirjavani <amirjavani@outlook.com>
2025-03-30 18:13:34 +0330

    burger menu with smooth transition implemented

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -2,41 +2,34 @@ import { Link } from "react-router";
 import frontcastLogo from "../assets/images/frontcast-logo-top.png";
 import { TfiMenu } from "react-icons/tfi";
 import { useRef, useState } from "react";
+import Navbar from "./Navbar";
 
 function Header() {
-    const [showMenu, setShowMenu] = useState(false);
-    
-    const showMenuToggle=()=>{
-        setShowMenu(!showMenu)
-    }
-    
-    useRef(null)
+  const [showMenu, setShowMenu] = useState(false);
+
+  const showMenuToggle = () => {
+    setShowMenu(!showMenu);
+  };
+
+  useRef(null);
 
   return (
-    <header className="pt-12">
-      <div className="container">
-        <nav className="flex flex-row justify-between items-center h-14 py-3 ">
-          <TfiMenu   onClick={()=>showMenuToggle()} className="show md:hidden text-[40px] text-[rgb(134,134,134)] py-0.5 px-2  border rounded cursor-pointer hover:bg-[rgb(240,239,239)]" />
-          <ul  className={`${showMenu?'flex flex-col relative top-30 bg-[rgb(134,134,134)] p-4 w-full':'hidden'} md:flex  md:flex-row gap-5 lg:gap-8 text-[16px] container font-bold `}>
-            <li>
-              <Link
-                to="/login"
-                className="text-[rgb(248,249,250)] px-3 bg-[#1976D2] py-2 rounded-lg hover:bg-[#1564b3] transition-colors ">
-                حساب کاربری
-              </Link>
-            </li>
-            <li className="hover:text-[#1976D2]  transition-colors">
-              <Link to="">صفحه اصلی</Link>
-            </li>
-            <li className="hover:text-[#1976D2]  transition-colors">
-              <Link to="">دوره‌های آموزشی</Link>
-            </li>
-            <li className="hover:text-[#1976D2]  transition-colors">
-              <Link to="">سبد خرید</Link>
-            </li>
-          </ul>
+    <header className={`pt-12 `}>
+      <div className="container ">
+        <nav className="flex  flex-row justify-between items-center h-14 py-3 ">
+          <TfiMenu
+            onClick={() => showMenuToggle()}
+            className="show md:hidden text-[40px] text-[rgb(134,134,134)] py-0.5 px-2  border rounded cursor-pointer hover:bg-[rgb(240,239,239)]"
+          />
+          <Navbar className={"flex-row md:flex hidden"} />
           <img className="h-full" src={frontcastLogo} alt="" />
         </nav>
+        <div
+          className={`transition-[100px] duration-300 ease-in-out overflow-hidden md:hidden 
+            ${showMenu ? "max-h-96" : "max-h-0"}
+            `}>
+          <Navbar className="flex flex-col bg-gray-100 p-4 rounded-md" />
+        </div>
       </div>
     </header>
   );
--- src/Layouts/Navbar.tsx
+++ src/Layouts/Navbar.tsx
@@ -1,11 +1,32 @@
+import { Link } from "react-router";
 
-
-function Navbar() {
+function Navbar ({className}: {className:string}) {
   return (
-    <div>
-      
-    </div>
-  )
+    <ul
+      className={`${
+        className
+        // showMenu
+        //   ? "flex flex-col  top-30 bg-[rgb(134,134,134)] p-4 w-full"
+        //   : "hidden"
+      }     gap-5 lg:gap-8 text-[16px] font-bold `}>
+      <li>
+        <Link
+          to="/login"
+          className="text-[rgb(248,249,250)] px-3 bg-[#1976D2] py-2 rounded-lg hover:bg-[#1564b3] transition-colors ">
+          حساب کاربری
+        </Link>
+      </li>
+      <li className="hover:text-[#1976D2]  transition-colors">
+        <Link to="">صفحه اصلی</Link>
+      </li>
+      <li className="hover:text-[#1976D2]  transition-colors">
+        <Link to="">دوره‌های آموزشی</Link>
+      </li>
+      <li className="hover:text-[#1976D2]  transition-colors">
+        <Link to="">سبد خرید</Link>
+      </li>
+    </ul>
+  );
 }
 
-export default Navbar
+export default Navbar;
