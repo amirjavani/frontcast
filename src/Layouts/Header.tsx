b8d49d1e7a651e675e2d1c26ce9b149aea400cab
amirjavani <amirjavani@outlook.com>
2025-03-30 17:28:44 +0330

    header styled

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -1,10 +1,45 @@
+import { Link } from "react-router";
+import frontcastLogo from "../assets/images/frontcast-logo-top.png";
+import { TfiMenu } from "react-icons/tfi";
+import { useRef, useState } from "react";
 
 function Header() {
+    const [showMenu, setShowMenu] = useState(false);
+    
+    const showMenuToggle=()=>{
+        setShowMenu(!showMenu)
+    }
+    
+    useRef(null)
+
   return (
-    <div>
-      Header
-    </div>
-  )
+    <header className="pt-12">
+      <div className="container">
+        <nav className="flex flex-row justify-between items-center h-14 py-3 ">
+          <TfiMenu   onClick={()=>showMenuToggle()} className="show md:hidden text-[40px] text-[rgb(134,134,134)] py-0.5 px-2  border rounded cursor-pointer hover:bg-[rgb(240,239,239)]" />
+          <ul  className={`${showMenu?'flex flex-col relative top-30 bg-[rgb(134,134,134)] p-4 w-full':'hidden'} md:flex  md:flex-row gap-5 lg:gap-8 text-[16px] container font-bold `}>
+            <li>
+              <Link
+                to="/login"
+                className="text-[rgb(248,249,250)] px-3 bg-[#1976D2] py-2 rounded-lg hover:bg-[#1564b3] transition-colors ">
+                حساب کاربری
+              </Link>
+            </li>
+            <li className="hover:text-[#1976D2]  transition-colors">
+              <Link to="">صفحه اصلی</Link>
+            </li>
+            <li className="hover:text-[#1976D2]  transition-colors">
+              <Link to="">دوره‌های آموزشی</Link>
+            </li>
+            <li className="hover:text-[#1976D2]  transition-colors">
+              <Link to="">سبد خرید</Link>
+            </li>
+          </ul>
+          <img className="h-full" src={frontcastLogo} alt="" />
+        </nav>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
--- src/Layouts/Layout.tsx
+++ src/Layouts/Layout.tsx
@@ -4,14 +4,21 @@ import Footer from "./Footer";
 import { Route, Routes } from "react-router";
 
 function Layout() {
+  const darkmode = false;
   return (
-    <>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<>sss</>} />
-      </Routes>
-      <Footer />
-    </>
+    <div
+      dir="rtl"
+      className={`main-font  ${
+        darkmode ? "bg-[#414757] text-[#f8f9fa]" : "bg-[#f8f9fa] text-[#212121]"
+      }   transition-all`}>
+      <div className="container px-10 mx-auto lg:px-20">
+        <Header />
+        <Routes>
+          <Route path="/" element={<>Home</>} />
+        </Routes>
+        <Footer />
+      </div>
+    </div>
   );
 }
 
